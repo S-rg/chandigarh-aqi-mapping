5a313ca74bc109b8ef98198f945eacc185a8b110
= <=>
2025-09-09 18:38:54 +0530

    Fixed bash script

--- update-webpage.sh
+++ update-webpage.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-REPO_DIR="/Desktop/chandigarh-aqi-mapping"
+REPO_DIR="."
 DEPLOY_DIR="/var/www/flaskapp"
 
 cd "$REPO_DIR" || exit
 git fetch origin main
 git reset --hard origin/main
+
+sudo chmod +x ./update-webpage.sh
 
 rsync -av --delete --exclude='.env' "$REPO_DIR/webapp/" "$DEPLOY_DIR/"
